60636d4cce562bb74b3c8d92c97c643455d0595c
tekin54 <oeztuerk.g54@gmail.com>
2025-11-05 21:45:21 +0100

    again

--- server/boilerplate/db/index.js
+++ server/boilerplate/db/index.js
@@ -1,19 +1,18 @@
 import pg from 'pg';
 
+// workaround for dates
 const DATE_OID = 1082;
 pg.types.setTypeParser(DATE_OID, (value) => value);
 
+// create pool with SSL + IPv4
 export const pool = new pg.Pool({
   host: process.env.PGHOST,
   user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
   database: process.env.PGDATABASE,
   port: process.env.PGPORT,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-  // Erzwinge IPv4
-  family: 4,
+  ssl: { rejectUnauthorized: false }, // Supabase erfordert SSL
+  family: 4, // zwingt IPv4, ENETUNREACH vermeiden
 });
 
 export const query = (text, params) => pool.query(text, params);
